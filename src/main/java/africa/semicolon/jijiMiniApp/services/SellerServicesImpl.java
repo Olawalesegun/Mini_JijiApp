4ea2e5fbdae90d5b7706c0e984f7d1506c71e8d7
Olawalesegun <olawalesegun0@gmail.com>
2023-07-17 22:32:34 +0100

    The Seller Services Impl class implements the Seller Services interface

--- src/main/java/africa/semicolon/jijiMiniApp/services/SellerServicesImpl.java
+++ src/main/java/africa/semicolon/jijiMiniApp/services/SellerServicesImpl.java
@@ -1,2 +1,10 @@
-package africa.semicolon.jijiMiniApp.services;public class SellerServicesImpl {
+package africa.semicolon.jijiMiniApp.services;
+
+import africa.semicolon.jijiMiniApp.dtos.requests.PostProductRequest;
+import africa.semicolon.jijiMiniApp.dtos.responses.PostProductResponse;
+
+public class SellerServicesImpl implements SellerServices{
+    PostProductResponse postProduct(PostProductRequest postProductRequest){
+
+    }
 }
