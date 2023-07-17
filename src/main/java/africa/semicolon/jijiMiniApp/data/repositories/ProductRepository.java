2bb73f358def8eaa29f3ca000c13d8aff9c80d68
Olawalesegun <olawalesegun0@gmail.com>
2023-07-17 22:29:38 +0100

    Product Repository Interface, extends to MongoRepository to inherit the MongoRepository behaviours

--- src/main/java/africa/semicolon/jijiMiniApp/data/repositories/ProductRepository.java
+++ src/main/java/africa/semicolon/jijiMiniApp/data/repositories/ProductRepository.java
@@ -1,2 +1,7 @@
-package africa.semicolon.jijiMiniApp.data.repositories;public interface ProductRepository {
+package africa.semicolon.jijiMiniApp.data.repositories;
+
+import africa.semicolon.jijiMiniApp.data.models.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
 }
