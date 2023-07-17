c3c1ef00fa4e369116df298844ad768ebbbc4886
Olawalesegun <olawalesegun0@gmail.com>
2023-07-17 22:26:53 +0100

    Product Class, used Lombok to generate all methods needed

--- src/main/java/africa/semicolon/jijiMiniApp/data/models/Product.java
+++ src/main/java/africa/semicolon/jijiMiniApp/data/models/Product.java
@@ -1,2 +1,20 @@
-package africa.semicolon.jijiMiniApp.data.models;public class Product {
+package africa.semicolon.jijiMiniApp.data.models;
+
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document("Products")
+public class Product {
+    @Id
+    private String id;
+    private String productName;
+    private String description;
+    @CreatedDate
+    private LocalDateTime datePosted;
 }
