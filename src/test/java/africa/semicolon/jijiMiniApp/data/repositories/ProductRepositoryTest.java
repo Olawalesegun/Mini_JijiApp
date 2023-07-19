0e381f2f3e4685ad93b77afe15f6cd224efa548a
Olawalesegun <olawalesegun0@gmail.com>
2023-07-19 21:50:58 +0100

    Product Repository Test

--- src/test/java/africa/semicolon/jijiMiniApp/data/repositories/ProductRepositoryTest.java
+++ src/test/java/africa/semicolon/jijiMiniApp/data/repositories/ProductRepositoryTest.java
@@ -1,6 +1,26 @@
 package africa.semicolon.jijiMiniApp.data.repositories;
 
+import africa.semicolon.jijiMiniApp.data.models.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+@DataMongoTest
 class ProductRepositoryTest {
-  
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void saveTest(){
+        Product newProduct = new Product();
+        Product savedProduct = productRepository.save(newProduct);
+        assertThat(productRepository.count(), is(1L));
+        assertEquals(1, productRepository.count());
+        assertNotNull(savedProduct.getId());
+    }
+
 }
